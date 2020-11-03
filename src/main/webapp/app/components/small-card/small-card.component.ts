94f50594c102887c03ae352a2219b8b6abe5580b
Daniel José Herrera Peraza <danieljhp@gmail.com>
2020-11-02 20:13:21 -0600

    Add template and component test
    Add small-card component.
    Add template references.
    Add template images.
    Add template scss.
    Add template js.
    
    Co-Authored-By: Achio9512 <63925818+Achio9512@users.noreply.github.com>
    Co-Authored-By: FernandoSolano20 <48603523+FernandoSolano20@users.noreply.github.com>
    Co-Authored-By: ajuarezm <28468017+ajuarezm@users.noreply.github.com>
    Co-Authored-By: Sergio Alfaro <39069474+wesbrek@users.noreply.github.com>

---
+++ src/main/webapp/app/components/small-card/small-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['../../../content/scss/paper-dashboard.scss']
+})
+export class SmallCardComponent implements OnInit {
+  @Input() buttonConfig: any;
+  @Input() description: any;
+  @Input() icon: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

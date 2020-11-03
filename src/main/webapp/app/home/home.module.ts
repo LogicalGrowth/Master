c5eb7937ac025a4a59d69dee0245cdadfc37b522
Fernando Solano <fersolano@ex2outcoding.local>
2020-11-03 01:26:48 -0600

    Add icon component

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { Fun4FundSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { IconComponent } from '../components/icon/icon.component';
 
 @NgModule({
   imports: [Fun4FundSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, IconComponent],
 })
 export class Fun4FundHomeModule {}

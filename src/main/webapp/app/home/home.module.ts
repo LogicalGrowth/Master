4d90ee0de9ecfb0287a698b5c14f767cecdfd885
Fernando Solano <fersolano@ex2outcoding.local>
2020-11-03 19:09:24 -0600

    Vertical component

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,10 +4,9 @@ import { RouterModule } from '@angular/router';
 import { Fun4FundSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import { IconComponent } from '../components/icon/icon.component';
 
 @NgModule({
   imports: [Fun4FundSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent, IconComponent],
+  declarations: [HomeComponent],
 })
 export class Fun4FundHomeModule {}

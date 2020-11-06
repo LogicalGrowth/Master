4fed9247212dade03fee8a7395acba2bb8fba513
Fernando Solano <fersolano@ex2outcoding.local>
2020-11-05 18:36:01 -0600

    Add nouislider on typescript, slider bar component and then add timeline component

---
+++ src/main/webapp/app/components/timeline/timeline.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['../../../content/scss/paper-dashboard.scss'],
+})
+export class TimelineComponent implements OnInit {
+  @Input() title: any;
+  @Input() cards: any;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { Fun4FundSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { TimelineComponent } from '../components/timeline/timeline.component';
 
 @NgModule({
   imports: [Fun4FundSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, TimelineComponent],
 })
 export class Fun4FundHomeModule {}

1689f62f0cb5ed51b7705f1340a97ebf277fceff
Fernando Solano <fersolano@ex2outcoding.local>
2020-11-06 20:11:29 -0600

    Add component to module

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,10 +4,9 @@ import { RouterModule } from '@angular/router';
 import { Fun4FundSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import { TimelineComponent } from '../components/timeline/timeline.component';
 
 @NgModule({
   imports: [Fun4FundSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent, TimelineComponent],
+  declarations: [HomeComponent],
 })
 export class Fun4FundHomeModule {}

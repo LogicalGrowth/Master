d82b5f34a3a266b94aac79d50718889095d855e0
Fernando Solano <fersolano@ex2outcoding.local>
2020-11-10 01:33:56 -0600

    Get the home page

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { Fun4FundSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { ComponentsModule } from 'app/components/components.module';
 
 @NgModule({
-  imports: [Fun4FundSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [Fun4FundSharedModule, RouterModule.forChild([HOME_ROUTE]), ComponentsModule],
   declarations: [HomeComponent],
 })
 export class Fun4FundHomeModule {}

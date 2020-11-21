714e93931929be33ac028d1b8d33018f175c76ee
ajuarez <ana.juarez@gslate.com>
2020-11-20 19:43:12 -0600

    Tweaks al lister proyecto

--- src/main/webapp/app/entities/proyect/proyect.component.ts
+++ src/main/webapp/app/entities/proyect/proyect.component.ts
@@ -8,6 +8,9 @@ import { IProyect } from 'app/shared/model/proyect.model';
 import { ProyectService } from './proyect.service';
 import { ProyectDeleteDialogComponent } from './proyect-delete-dialog.component';
 import * as moment from 'moment';
+import { User } from '../../core/user/user.model';
+import { AuctionService } from '../auction/auction.service';
+import { AccountService } from '../../core/auth/account.service';
 
 @Component({
   selector: 'jhi-proyect',
@@ -19,8 +22,15 @@ export class ProyectComponent implements OnInit, OnDestroy {
   eventSubscriber?: Subscription;
   updatedDays: any;
   percentile: any;
+  isProjectOwner!: Boolean;
+  account!: User;
 
-  constructor(protected proyectService: ProyectService, protected eventManager: JhiEventManager, protected modalService: NgbModal) {}
+  constructor(
+    protected proyectService: ProyectService,
+    protected eventManager: JhiEventManager,
+    protected modalService: NgbModal,
+    private accountService: AccountService
+  ) {}
 
   loadAll(): void {
     this.proyectService.query().subscribe((res: HttpResponse<IProyect[]>) => (this.proyects = res.body || []));
@@ -29,6 +39,12 @@ export class ProyectComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.loadAll();
     this.registerChangeInProyects();
+
+    this.accountService.identity().subscribe(account => {
+      if (account) {
+        this.account = account;
+      }
+    });
   }
 
   ngOnDestroy(): void {
@@ -59,5 +75,9 @@ export class ProyectComponent implements OnInit, OnDestroy {
     if (item.collected && item.goalAmount) {
       return (100 * item.collected) / item.goalAmount;
     }
+  }
+
+  isProjectAdmin(item: IProyect): boolean {
+    return this.account.id === item?.owner?.id ? true : false;
   }
 }

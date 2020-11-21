8d8bc053c4fe967517b952b008a26de01e104445
Sergio Alfaro <sergioalfaro2807@gmail.com>
2020-11-20 23:44:42 -0600

    Payment with email done

--- src/main/webapp/app/components/paypal-button/paypal-button.component.ts
+++ src/main/webapp/app/components/paypal-button/paypal-button.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { PaymentService } from 'app/entities/payment/payment.service';
 
+import { IPayment, Payment } from 'app/shared/model/payment.model';
 declare let paypal: any;
+import { ProductType } from 'app/shared/model/enumerations/product-type.model';
+import { AccountService } from 'app/core/auth/account.service';
+import { User } from 'app/core/user/user.model';
+import * as moment from 'moment';
+import { IProyect } from '../../shared/model/proyect.model';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-paypal-button',
@@ -12,27 +21,32 @@ export class PaypalButtonComponent implements OnInit {
 
   @Input() description: string;
   @Input() price: string;
-
+  @Input() projectId: string;
+  @Input() productType?: ProductType;
   finalPrice = 0;
   paidFor = false;
+  idUser = '';
+  account!: User;
+  projectIdFormat?: number;
+  productTypeFormat?: IPayment;
 
-  product = {
-    price: 777.77,
-    description: 'used couch, decent condition',
-    img: 'https://picsum.photos/id/237/200/300',
-  };
-
-  constructor(paypalElement: ElementRef) {
+  constructor(paypalElement: ElementRef, protected paymentService: PaymentService, private accountService: AccountService) {
     // Initialization inside the constructor
     this.paypalElement = paypalElement;
     this.description = '';
     this.price = '';
+    this.projectId = '';
   }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
     this.finalPrice = +this.price.replace(/,/, '.');
-
+    this.projectIdFormat = parseInt(this.projectId, 10);
+    this.accountService.identity().subscribe(account => {
+      if (account) {
+        this.account = account;
+      }
+    });
     paypal
       .Buttons({
         createOrder: (data: any, actions: any) => {
@@ -52,8 +66,29 @@ export class PaypalButtonComponent implements OnInit {
         onApprove: async (data: any, actions: { order: { capture: () => any } }) => {
           const order = await actions.order.capture();
           this.paidFor = true;
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          const payment = this.createPayment(order.create_time);
+          this.subscribeToSaveResponse(this.paymentService.createWithoutFormat(payment));
         },
       })
       .render(this.paypalElement.nativeElement);
+  }
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  private createPayment(create_time: any): IPayment {
+    return {
+      ...new Payment(),
+      amount: this.finalPrice,
+      applicationUser: this.account,
+      proyect: {
+        id: this.projectIdFormat,
+      },
+      type: this.productType,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      timeStamp: create_time,
+    };
+  }
+
+  protected subscribeToSaveResponse(result: Observable<HttpResponse<IPayment>>): void {
+    result.subscribe();
   }
 }

f83fb17d31d19692daf960f5d1fbe0fe4181be72
Fernando Solano <fersolano@ex2outcoding.local>
2020-11-21 21:22:51 -0600

    NO TICKET Order by ascending

--- src/main/java/cr/ac/ucenfotec/fun4fund/web/rest/CheckpointResource.java
+++ src/main/java/cr/ac/ucenfotec/fun4fund/web/rest/CheckpointResource.java
@@ -12,6 +12,7 @@ import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -147,6 +148,6 @@ public class CheckpointResource {
         log.debug("REST request to get Checkpoint : {}", idproyect);
         Long id = Long.parseLong(idproyect);
         Double per = Double.parseDouble(percentile);
-        return checkpointRepository.findByProyectIdAndCompletitionPercentageLessThanEqual(id,per);
+        return checkpointRepository.findByProyectIdAndCompletitionPercentageLessThanEqual(id,per, Sort.by("completitionPercentage").ascending());
     }
 }

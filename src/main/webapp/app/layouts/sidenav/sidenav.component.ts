541456ffe7e79fb434e96298e5cf66faae4be3b5
Daniel José Herrera Peraza <danieljhp@gmail.com>
2020-12-08 00:36:22 -0600

    Add proyect management route to navbar.

--- src/main/webapp/app/layouts/sidenav/sidenav.component.ts
+++ src/main/webapp/app/layouts/sidenav/sidenav.component.ts
@@ -21,7 +21,7 @@ export class SidenavComponent implements OnInit, OnDestroy {
     { name: 'Inicio', route: '/', icon: '../../../content/images/navIcons/house.png' },
     { name: 'Usuarios', route: '/application-user', icon: '../../../content/images/navIcons/users.png' },
     { name: 'Proyectos', route: '/proyect', icon: '../../../content/images/navIcons/project.png' },
-    { name: 'Administración de proyectos', route: '/proyect/proyectManagement', icon: '../../../content/images/navIcons/project.png' },
+    { name: 'Administrar proyectos', route: '/proyect/proyectManagement', icon: '../../../content/images/navIcons/gear.png' },
     { name: 'Configuración', route: '/admin/bank-account', icon: '../../../content/images/navIcons/gear.png' },
     { name: 'Categorías', route: '/category', icon: '../../../content/images/navIcons/category.png' },
   ];
